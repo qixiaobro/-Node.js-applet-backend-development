4c3bd5cd0a87422059c12a5770a48c44743989f8
qixiaobro <982092690@qq.com>
2022-07-18 22:24:27 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,72 @@
+export default{
+  title: '基于 hapi 的 Node.js 小程序后端开发实践指南',
+  description: '前端通往全栈第一课',
+  themeConfig:{
+    sidebar: [
+      {
+        text: '课程',
+        items: [
+          {
+            text: '介绍',
+            link: '/01',
+          },
+          {
+            text: '需求分析与基础设计',
+            link: '/02',
+          },
+          {
+            text: '后端技术选型-node.js',
+            link: '/03',
+          },
+          {
+            text: '项目工程初始化-hapi',
+            link: '/04',
+          },
+          {
+            text: '接口契约与入参校验-swagger',
+            link: '/05',
+          },
+          {
+            text: '表结构设计-sequelize',
+            link: '/06',
+          },
+          {
+            text: '小程序列表获取',
+            link: '/07',
+          },
+          {
+            text: '身份验证设计',
+            link: '/08',
+          },{
+            text: '身份验证实现',
+            link: '/09',
+          },
+          {
+            text: '小程序登录授权与JWT签发',
+            link: '/10',
+          },{
+            text: '小程序订单创建（事务）',
+            link: '/11',
+          },
+          {
+            text: '小程序订单支付（微信支付）',
+            link: '/12',
+          },{
+            text: '服务部署发布',
+            link: '/13',
+          },
+          {
+            text: '系统监控与记录',
+            link: '/14',
+          },{
+            text: '系统稳定性测试',
+            link: '/15',
+          },{
+            text: '项目回顾',
+            link: '/16',
+          },
+        ],
+      },
+    ]
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+---
+layout: home
+sidebar: false
+
+title: Node.js开发小程序后端
+titleTemplate: 基于hapi的 Node.js 小程序后端开发实践指南
+
+hero:
+  name: Node.js开发小程序后端
+  text: 基于hapi的 Node.js 小程序后端开发实践指南
+  tagline: 超级简单，包教包会！
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /01
+---
